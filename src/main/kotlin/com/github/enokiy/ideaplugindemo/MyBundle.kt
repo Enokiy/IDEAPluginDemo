6d9f682044db2b3b10198ac07e10786f0932a085
GitHub Action <action@github.com>
2025-01-22 06:07:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/enokiy/ideaplugindemo/MyBundle.kt
+++ src/main/kotlin/com/github/enokiy/ideaplugindemo/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.enokiy.ideaplugindemo
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/enokiy/ideaplugindemo/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/enokiy/ideaplugindemo/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.enokiy.ideaplugindemo.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
